301c152f8e27f63e9f15b36c429b5b5d05ec751c
ducqhl <duc.le@spiderbox.design>
2022-11-18 02:01:21 +0700

    update title

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
       <div class="left">
         <div class="col-1">
           <router-link class="header" :to="{ name: ROUTE_NAMES.HOME }"
-            >FireBlogs</router-link
+            >GreenLogs</router-link
           >
           <ul>
             <li>
