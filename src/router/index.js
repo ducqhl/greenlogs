8cb1fa10f7bbc01c7cdf9962f7d7324162b4fd18
ducqhl <duc.le@spiderbox.design>
2022-11-18 02:07:57 +0700

    change auto scroll top

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   base: import.meta.env.BASE_URL,
   scrollBehavior() {
-    return { x: 0, y: 0 };
+    return { top: 0 };
   },
   routes: [
     {
